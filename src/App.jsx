e0124324cd290b955a98fc93ea58ced993d5a085
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-09-23 11:42:03 -0300

    agregamos el state del personaje y se carga la pagina correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container } from 'react-bootstrap';
 import logo from './assets/logosimpson.png'
 import Frase from './components/Frase.jsx'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
-
+    const [personaje, setPersonaje] = useState({})
     useEffect(()=>{
       consultarAPI();
     }, [])
@@ -14,13 +14,14 @@ function App() {
       //solicitud GET (POST,PUT O PATCH, DELETE)
       const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
       const datos = await respuesta.json()
-      console.log(datos)
+      console.log(datos[0])
+      setPersonaje(datos[0])
     }
   return (
     <>
       <Container className='text-center my-4'>
         <img src={logo} alt="Logo de los simpsons" className='w-75' />
-        <Frase></Frase>
+        <Frase personaje={personaje}></Frase>
         <Button variant='warning' className='mt-3'>Obtener Frase</Button>
       </Container>
     </>
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,16 +1,15 @@
 import { Card } from "react-bootstrap";
 
 
-const Frase = () => {
+const Frase = ({personaje}) => {
     return (
         <section>
-            <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185" alt="Nelson Muntz" />
+            <img src={personaje.image} alt={personaje.character} />
             <Card className="mt-3">
                  <Card.Body>
-                    <Card.Title>Nelson Muntz</Card.Title>
+                    <Card.Title>{personaje.character}</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
+                            {personaje.quote}
                         </Card.Text>
                      </Card.Body>
                 </Card>
